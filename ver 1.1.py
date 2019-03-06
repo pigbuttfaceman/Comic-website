7095eedc15da7051fad9d4ec9a2e73b76b110380
nugg man <37384979+pigbuttfaceman@users.noreply.github.com>
2019-03-06 15:11:01 +1300

    Add files via upload

---
+++ ver 1.1.py
@@ -0,0 +1,36 @@
+from bottle import run, route, view, get, post, request
+from itertools import count
+
+
+
+class Comic:
+
+    _ids = count (0)
+
+    def __init__(self, name, stock, image):
+        self.id = next(self._ids)
+        self.comic_name = name
+        self.comic_image = image
+        self.comic_stock = stock
+
+
+
+comics = [
+    Comic("Super Dude", "image", 8 ),
+    Comic("Lizard Man", "image", 12 ),
+    Comic("Water Woman", "image", 3)
+    ]
+
+
+#index page
+@route("/")
+@view ("index")
+def index():
+
+    pass
+
+
+
+run(host='0.0.0.0', port=8080, reloader = True, debug = True)
+
+
